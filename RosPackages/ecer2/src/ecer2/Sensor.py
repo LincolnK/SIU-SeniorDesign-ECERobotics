63c65d6334d5d1c823b70682f46abdcdffa2530e
F18-21 ECER2 <>
2018-10-25 18:00:37 +0000

    updated project dependencies

--- RosPackages/ecer2/src/ecer2/Sensor.py
+++ RosPackages/ecer2/src/ecer2/Sensor.py
@@ -8,12 +8,15 @@ class Sensor(object):
     def start(self):
         if not GPIO.getmode():
             GPIO.setmode(GPIO.BCM)
-            self.current = GPIO.input(self._line)
+            self._current = GPIO.input(self._line)
         GPIO.setup(line,GPIO.IN)
         GPIO.add_event_detect(self._line,GPIO.BOTH)
         GPIO.add_event_callback(self._line,self._cb)
+    @property
+    def value(self):
+        return self._current
     def _cb(self,channel):
-        self.current = GPIO.input(self._line)
+        self._current = GPIO.input(self._line)
     def __enter__(self):
         self.start()
         return self
