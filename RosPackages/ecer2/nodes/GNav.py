34efae03aff0fddc8c46722f790ac44a118ff0e8
F18-21 ECER2 <>
2018-11-27 20:53:19 +0000

    bugfixing

--- RosPackages/ecer2/nodes/GNav.py
+++ RosPackages/ecer2/nodes/GNav.py
@@ -5,8 +5,8 @@ import rospy
 import tf2_ros
 import traceback
 from tf2_geometry_msgs import do_transform_point
-from tf.transformations import quaternion_from_euler
-from geometry_msgs.msg import Twist, PoseStamped
+from tf.transformations import quaternion_from_euler,euler_from_quaternion
+from geometry_msgs.msg import Twist, PoseStamped,Quaternion
 from sensor_msgs.msg import LaserScan
 
 GOAL_TOLERANCE=0.1
@@ -16,6 +16,8 @@ SPEED = 0.5
 tf2_buffer = tf2_ros.Buffer()
 tf2_listener = tf2_ros.TransformListener(tf2_buffer)
 command_publisher = rospy.Publisher("cmd_vel",Twist, queue_size=10)
+angle_adjustment=None
+"""Difference between lidar and base_link"""
 
 def getCurrentPoint():
     t = tf2_buffer.lookup_transform("map","base_link",rospy.Time())
@@ -48,6 +50,7 @@ def sendCommand(angle):
         command.linear.x=0.
         command.linear.y=0.
     else:
+        print "sending angle ",angle
         command.linear.x = cos(angle)
         command.linear.y = sin(angle)
     command_publisher.publish(command)
@@ -55,7 +58,6 @@ def sendCommand(angle):
 
 def calc_command():
     global tf2_listener
-    command = Twist()
     vec = (0.,0.,0.)
     found_goal = False
     try:
@@ -69,7 +71,7 @@ def calc_command():
         print "pos is ",pos
         print "goal is", current_goal
         dist = sqrt( (pos[0]-current_goal[0])**2 + (pos[1]-current_goal[1])**2)
-        left_angle = acos((current_goal[0]-pos[0])/dist)
+        left_angle = acos((current_goal[0]-pos[0])/dist) + angle_adjustment
         right_angle = left_angle
         print "initial angle",left_angle
         increment=0.075
@@ -108,10 +110,20 @@ def listen_goal(data):
 
 
 def main_loop():
+    global angle_adjustment
     rospy.init_node("greedy_nav")
+    recalc_rate = rospy.Rate(2)
+    while angle_adjustment is None: 
+        try:
+            t = tf2_buffer.lookup_transform("base_link","lidar",rospy.Time())
+            print t.transform.rotation
+            a = euler_from_quaternion(Quaternion(t.transform.rotation.x,t.transform.rotation.y,t.transform.rotation.z, t.transform.rotation.w))
+            angle_adjustment=a.z
+        except:
+            traceback.print_exc()
+            rospy.sleep(0.1)
     rospy.Subscriber("move_base_simple/goal",PoseStamped, listen_goal)
     rospy.Subscriber("scan",LaserScan,listen_scan)
-    recalc_rate = rospy.Rate(2)
     while not rospy.is_shutdown():
         calc_command()
         recalc_rate.sleep()
